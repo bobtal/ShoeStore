fa0fdc9ff64e41b450bb6a429247f658ecd9cfa6
bobtal <talevski.boban@gmail.com>
2021-01-21 22:11:39 +0100

    Updates navigation to use Android KTX

--- app/src/main/java/com/udacity/shoestore/LoginFragment.kt
+++ app/src/main/java/com/udacity/shoestore/LoginFragment.kt
@@ -5,9 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.findNavController
+import androidx.navigation.Navigation
 import com.udacity.shoestore.databinding.FragmentLoginBinding
 
 /**
@@ -15,7 +14,7 @@ import com.udacity.shoestore.databinding.FragmentLoginBinding
  * Use the [LoginFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class LoginFragment : Fragment(), View.OnClickListener {
+class LoginFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,15 +25,16 @@ class LoginFragment : Fragment(), View.OnClickListener {
             inflater, R.layout.fragment_login, container, false
         )
 
-        // Add same onClickListener for both buttons
-        binding.loginButton.setOnClickListener(this)
-        binding.signupButton.setOnClickListener(this)
+        // create a single onClickListener to be used by both buttons since
+        // destination is the same
+        val goToWelcomeScreenOnClick =
+            Navigation.createNavigateOnClickListener(R.id.action_loginFragment_to_welcomeFragment)
+
+        // Add the same onClickListener for both buttons
+        binding.loginButton.setOnClickListener(goToWelcomeScreenOnClick)
+        binding.signupButton.setOnClickListener(goToWelcomeScreenOnClick)
 
         return binding.root
-    }
-
-    override fun onClick(v: View?) {
-        v?.findNavController()?.navigate(R.id.action_loginFragment_to_welcomeFragment)
     }
 
 }
--- app/src/main/java/com/udacity/shoestore/WelcomeFragment.kt
+++ app/src/main/java/com/udacity/shoestore/WelcomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.databinding.FragmentWelcomeBinding
 
@@ -25,9 +26,8 @@ class WelcomeFragment : Fragment() {
             inflater, R.layout.fragment_welcome, container, false)
 
         // setup the onClickListener for the button for navigation
-        binding.toInstructionsButton.setOnClickListener{
-            findNavController().navigate(R.id.action_welcomeFragment_to_instructionsFragment)
-        }
+        binding.toInstructionsButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_welcomeFragment_to_instructionsFragment))
         return binding.root
     }
 
