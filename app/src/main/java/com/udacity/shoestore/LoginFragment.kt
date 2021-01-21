1d2648d5f78c99277d79934787487bc4a5680a39
bobtal <talevski.boban@gmail.com>
2021-01-21 21:39:54 +0100

    Completes navigation from LoginFragment to WelcomeFragment

--- app/src/main/java/com/udacity/shoestore/LoginFragment.kt
+++ app/src/main/java/com/udacity/shoestore/LoginFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.udacity.shoestore.databinding.FragmentLoginBinding
 
 /**
@@ -13,14 +15,26 @@ import com.udacity.shoestore.databinding.FragmentLoginBinding
  * Use the [LoginFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class LoginFragment : Fragment() {
+class LoginFragment : Fragment(), View.OnClickListener {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
-        val binding : FragmentLoginBinding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_login, container, false)
+        val binding: FragmentLoginBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_login, container, false
+        )
+
+        // Add same onClickListener for both buttons
+        binding.loginButton.setOnClickListener(this)
+        binding.signupButton.setOnClickListener(this)
+
         return binding.root
+    }
+
+    override fun onClick(v: View?) {
+        v?.findNavController()?.navigate(R.id.action_loginFragment_to_welcomeFragment)
     }
 
 }
