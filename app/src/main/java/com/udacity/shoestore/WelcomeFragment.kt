5d839e128bc7d04810ad0ad29b41e8a0ee0fda7b
bobtal <talevski.boban@gmail.com>
2021-01-21 22:02:34 +0100

    Completes navigation from WelcomeFragment to InstructionsFragment

--- app/src/main/java/com/udacity/shoestore/WelcomeFragment.kt
+++ app/src/main/java/com/udacity/shoestore/WelcomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.databinding.FragmentWelcomeBinding
 
 /**
@@ -22,6 +23,11 @@ class WelcomeFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding : FragmentWelcomeBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_welcome, container, false)
+
+        // setup the onClickListener for the button for navigation
+        binding.toInstructionsButton.setOnClickListener{
+            findNavController().navigate(R.id.action_welcomeFragment_to_instructionsFragment)
+        }
         return binding.root
     }
 
