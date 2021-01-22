5a1cecb621d4be3066a2c6fdc8f6102c58f75c92
bobtal <talevski.boban@gmail.com>
2021-01-22 22:46:54 +0100

    Adds the Log out menu in ShoeListFragment

--- app/src/main/java/com/udacity/shoestore/ShoeListFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ShoeListFragment.kt
@@ -1,14 +1,14 @@
 package com.udacity.shoestore
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.udacity.shoestore.databinding.FragmentShoeListBinding
 import com.udacity.shoestore.viewmodel.ShoeListViewModel
 
@@ -27,10 +27,23 @@ class ShoeListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val binding: FragmentShoeListBinding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_shoe_list, container, false
-        )
+            inflater, R.layout.fragment_shoe_list, container, false)
+
+        setHasOptionsMenu(true)
 
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.logout_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.loginFragment->
+                return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
