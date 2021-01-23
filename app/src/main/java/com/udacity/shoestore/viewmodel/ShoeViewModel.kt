3f3db7ef154a573fad450b69bc32bee9b54d66d5
bobtal <talevski.boban@gmail.com>
2021-01-23 23:14:44 +0100

    Adds ShoeViewModel.kt for two way data binding

---
+++ app/src/main/java/com/udacity/shoestore/viewmodel/ShoeViewModel.kt
@@ -0,0 +1,19 @@
+package com.udacity.shoestore.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.udacity.shoestore.models.Shoe
+
+class ShoeViewModel : ViewModel() {
+
+    private val _shoeLiveData = MutableLiveData<Shoe>()
+    val shoeLiveData: LiveData<Shoe>
+        get() = _shoeLiveData
+
+    // initialize an "empty" Shoe object as placeholder for the layout
+    init {
+        _shoeLiveData.value = Shoe("", 0.0, "", "")
+    }
+
+}
